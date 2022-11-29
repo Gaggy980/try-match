c81d74806da6f3cb1b526f2174c11a7824ccf26b
Igor G <igorgagicit@gmail.com>
2022-11-29 18:53:31 +0000

    heroes deck created

---
+++ src/database.js
@@ -0,0 +1,21 @@
+export const cardsFrozen = [
+  { src: "./elsa.png", matched: false },
+  { src: "./anna.png", matched: false },
+  { src: "./gecko.png", matched: false },
+  { src: "./hans.png", matched: false },
+  { src: "./kristoff.png", matched: false },
+  { src: "./sven.png", matched: false },
+  { src: "./olaf.png", matched: false },
+  { src: "./troll.png", matched: false },
+];
+
+export const cardsHeroes = [
+    { src: "./cameleon.png", matched: false },
+    { src: "./dpool.png", matched: false },
+    { src: "./groot.png", matched: false },
+    { src: "./hulk.png", matched: false },
+    { src: "./iron.png", matched: false },
+    { src: "./spidey.png", matched: false },
+    { src: "./venom.png", matched: false },
+    { src: "./wolv.png", matched: false },
+];
