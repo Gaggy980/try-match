3815f46de3bd80a38c56715d0f0de06e150071da
Igor G <igorgagicit@gmail.com>
2022-11-29 19:03:46 +0000

    problem with picture paths test

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     shuffleCards()
     setColorPallete("#242070", "#B5EFE8", "rgb(36,32,112,0.70)")
     toggleBack('url("back.png")')
-    toggleBgImage('url("bgfrozen.png")')
+    toggleBgImage('url("./public/bgfrozen.png")')
     setHeadTitle("FROZEN Memory")
     setHidden(true)
   }
@@ -32,7 +32,7 @@ function App() {
     setCardImages(cardsHeroes)
     shuffleCards()
     setColorPallete("#390100", "#D30C0F", "rgb(57,1,0,0.9)")
-    toggleBack('url("backHeros.png")')
+    toggleBack('url("./backHeros.png")')
     toggleBgImage('url("MarvelHeroes.png")')
     setHeadTitle("HEROES Memory")
     setHidden(true)
